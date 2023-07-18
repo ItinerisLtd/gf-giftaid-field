dbde18fdda94505b55ee4b58a60bd150e0be8e7b
codepuncher <lee.hanbury@gmail.com>
2023-07-18 15:47:53 +0100

    fix: wrong plugin URL

--- gf-giftaid-field.php
+++ gf-giftaid-field.php
@@ -25,7 +25,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 }
 
 if (! defined('ITINERIS_GF_GIFT_AID_FIELD_URI')) {
-    define('ITINERIS_GF_GIFT_AID_FIELD_URI', WPMU_PLUGIN_URL . '/' . basename(__DIR__));
+    define('ITINERIS_GF_GIFT_AID_FIELD_URI', plugins_url(basename(__DIR__)));
 }
 
 add_action('gform_loaded', [GfGiftAidField::class, 'load'], 5);
