73df154af687005e856caba043af7f69898646cd
Barny-Thorpe <bmcthorpe@gmail.com>
2024-02-29 11:36:31 +0000

    save js current

--- public/js/gf-giftaid-field-frontend.js
+++ public/js/gf-giftaid-field-frontend.js
@@ -1,57 +1,59 @@
-function gfGiftAidOnInputChange(elem) {
-  if (!(elem instanceof HTMLElement)) {
-    return;
-  }
-
-  if (! elem.classList.contains('ginput_total') && ! elem.classList.contains('ginput_amount')) {
-    return;
-  }
-
-  const parentForm = elem.closest('form');
-  if (!(parentForm instanceof HTMLFormElement)) {
-    return;
-  }
-
-  const totalEl = parentForm.querySelector('.ginput_total');
-  if (totalEl instanceof HTMLElement) {
-    elem = totalEl;
-  }
-
-  const spanTotal = parentForm.querySelectorAll('.gform_donation_total');
-  if (!(spanTotal instanceof NodeList) || 0 === spanTotal.length) {
-    return;
-  }
-
-  const spanTotalGift = parentForm.querySelectorAll(
-    '.gform_donation_gifttotal',
-  );
-  if (!(spanTotalGift instanceof NodeList) || 0 === spanTotalGift.length) {
-    return;
-  }
-
-  const extractFloat = (str) => {
-    // Regular expression to match floating-point numbers
-    const regex = /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/g;
-
-    // Use match() to find all matching numbers in the string
-    const matches = str.match(regex);
-
-    // If there are matches, convert them to floats and return them; otherwise, return the original str.
-    return matches ? matches.map(Number) : str;
-  };
-
-  const val = extractFloat(elem.value);
-  const total = parseFloat(val).toFixed(2);
-  const totalGift = parseFloat(val * 1.25).toFixed(2);
-
-  Array.from(spanTotal).forEach((item) => {
-    item.innerHTML = total;
-  });
-
-  Array.from(spanTotalGift).forEach((item) => {
-    item.innerHTML = totalGift;
-  });
-}
 document.addEventListener('DOMContentLoaded', () => {
-  window.gform.addAction('gform_input_change', gfGiftAidOnInputChange, 10);
+  updateGiftAidTotal();
 });
+
+function updateGiftAidTotal() {
+  console.log( window.gform);
+  window.gform.addAction('gform_input_change', updateGFFromTotal, 10);
+}
+
+function updateGFFromTotal(elem) {
+  const total = getTotalAmount(elem);
+  if (! total) return;
+
+  updateGiftAidDisplay(total);
+}
+
+/**
+ * Gets the total amount donated and updates the Gift Aid total.
+ * @param {HTMLElement} elem
+ * @returns {string} The total amount donated.
+ */
+function getTotalAmount(elem) {
+  if (! (elem instanceof HTMLInputElement) || ! elem.classList.contains('ginput_total')) {
+    return '';
+  }
+  const totalValueStr = elem.value;
+  const totalValueFloat = parseFloat(totalValueStr);
+  if (! totalValueFloat) {
+    return '';
+  }
+  const roundedTwoDP = totalValueFloat.toFixed(2);
+  if (! roundedTwoDP) {
+    return '';
+  }
+  return roundedTwoDP;
+}
+
+/**
+ * updates the gift aid display.
+ * @param {string} total
+ */
+function updateGiftAidDisplay(total) {
+  const gravityForm = document.querySelector('.gform_wrapper');
+  if (! (gravityForm instanceof HTMLElement)) {
+    return;
+  }
+  const spanTotal = gravityForm.querySelector('.gform_donation_total');
+  if (! (spanTotal instanceof HTMLSpanElement)) {
+    return;
+  }
+  const spanTotalGift = gravityForm.querySelector('.gform_donation_gifttotal');
+  if (! (spanTotalGift instanceof HTMLSpanElement)) {
+    return;
+  }
+  spanTotal.innerHTML = total;
+
+  const totalGift = parseFloat(total * 1.25).toFixed(2);
+  spanTotalGift.innerHTML = totalGift;
+}
