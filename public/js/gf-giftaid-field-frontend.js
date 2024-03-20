4dc97e85fa912e242ff3dbf99e08c009c4102361
Barny-Thorpe <bmcthorpe@gmail.com>
2024-03-20 15:57:09 +0000

    update js to match

--- public/js/gf-giftaid-field-frontend.js
+++ public/js/gf-giftaid-field-frontend.js
@@ -6,40 +6,52 @@
  */
 
 document.addEventListener('DOMContentLoaded', () => {
-  determineDonationTotalMethod();
+  initGiftAid();
 });
 
-function determineDonationTotalMethod() {
+/**
+ * Updates the gift aid display, based on a chosen field changing.
+ */
+function initGiftAid() {
+  const totalSelector = totalFieldSelector() || '.gfield.gfield--input-type-total';
+  window.gform.addAction('gform_input_change', (elem) => {
+    const total = getTotalAmount(elem, totalSelector);
+    if (! total || !total.donation || !total.giftAidTotal) {
+      return;
+    }
+    updateGiftAidDisplay(total);
+  }, 10);
+}
+
+/**
+ * Gets the selector of the total field, selected in the gift aid field settings.
+ * Will be in the format 'field_<form_id>_<field_id>' and will be an id so the selector will be prefixed by #.
+ * @returns {string} The class of the total field.
+ */
+function totalFieldSelector() {
   const giftAidComponent = document.querySelector('.gift-box-wrapper');
   if (! (giftAidComponent instanceof HTMLElement)) {
-    return;
+    return '';
   }
   const donationTotalInput = giftAidComponent.querySelector('input.donation-total-select');
   if (!(donationTotalInput instanceof HTMLInputElement)) {
-    return;
+    return '';
   }
-  const donationTotalSelection = donationTotalInput.value;
-  if ("ginput_total" === donationTotalSelection) {
-    window.gform.addAction('gform_input_change', updateGFFromTotal, 10);
+  const fieldId = donationTotalInput.value;
+  if (!fieldId) {
+    return '';
   }
-}
-
-function updateGFFromTotal(elem) {
-  const total = getTotalAmount(elem);
-  if (! total || !total.donation || !total.giftAidTotal) {
-    return;
-  }
-
-  updateGiftAidDisplay(total);
+  return `#${fieldId}`;
 }
 
 /**
  * Gets the total amount donated and updates the Gift Aid total.
- * @param {HTMLElement} elem
+ * @param {HTMLElement} elem The element that has changed.
+ * @param {string} totalSelector selector of the chosen total field.
  * @returns {Donation} The total amount donated.
  */
-function getTotalAmount(elem) {
-  if (! (elem instanceof HTMLInputElement) || ! elem.classList.contains('ginput_total')) {
+function getTotalAmount(elem, totalSelector) {
+  if (! (elem instanceof HTMLInputElement) || ! elem.closest(totalSelector)) {
     return '';
   }
   const totalValueStr = elem.value;
